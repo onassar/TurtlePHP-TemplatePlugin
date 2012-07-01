5465136cd74c8fb6bb08948a5690f86d8abbf2ca
Oliver Nassar <onassar@gmail.com>
2012-06-30 22:02:20 -0400

    Getter

--- Template.class.php
+++ Template.class.php
@@ -25,13 +25,20 @@
      * `Hello World!` after that page has been rendered programatically, but
      * before it's been flushed to the output stream.
      * 
-     * @todo     Add benchmarking (execution time for templating)
-     * @see      https://github.com/onassar/PHP-Template
-     * @author   Oliver Nassar <onassar@gmail.com>
-     * @abstract
+     * @todo   Add benchmarking (execution time for templating)
+     * @see    https://github.com/onassar/PHP-Template
+     * @author Oliver Nassar <onassar@gmail.com>
      */
-    abstract class Template
+    class Template
     {
+        /**
+         * _hash
+         * 
+         * @var    array
+         * @access protected
+         */
+        protected $_hash;
+
         /**
          * init
          * 
@@ -42,16 +49,19 @@
          * templating process.
          * 
          * @access public
-         * @static
+         * @param  Request $request
          * @return void
          */
-        public static function init()
+        public function __construct(\Turtle\Request $request)
         {
-            // retrieve request
-            $request = \Turtle\Application::getRequest();
+            // instance reference
+            $self = $this;
 
             // templating callback
-            $request->addCallback(function($buffer) {
+            $request->addCallback(function($buffer) use ($request, $self) {
+
+                // request-path header
+                $self->setPathHeader($request);
 
                 // start time
                 $start = microtime(true);
@@ -64,10 +74,48 @@
 
                 // pass as header
                 $duration = round($end - $start, 4);
-                header('TurtlePHP-Templating: ' . ($duration));
+                header(
+                    'TurtlePHP-' . ($self->getHash()) . '-Templating: ' .
+                    ($duration)
+                );
 
                 // return rendered
                 return $rendered;
             });
+        }
+
+        /**
+         * getHash
+         * 
+         * @access public
+         * @return String
+         */
+        public function getHash()
+        {
+            return $this->_hash;
+        }
+
+        /**
+         * setPathHeader
+         * 
+         * @access public
+         * @param  Request $request
+         * @return void
+         */
+        public function setPathHeader(\Turtle\Request $request)
+        {
+            // set path (for header passing)
+            $route = $request->getRoute();
+            $path = $route['path'];
+
+            // grab md5 and truncate it
+            $md5 = md5($path);
+            $md5 = substr($md5, 0, 6);
+
+            // set instance md5
+            $this->_hash = $md5;
+
+            // set path header
+            header('TurtlePHP-' . ($md5) . ': ' . ($path));
         }
     }
